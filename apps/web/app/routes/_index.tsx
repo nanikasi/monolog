12ad5af50892d5ed0b7fb66fcf38b060ff659e01
nanikasi <nanikasi1024@gmail.com>
2024-08-09 06:55:32 +0000

    Remove links and Fix title

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -3,10 +3,10 @@ import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Monolog" },
     {
       name: "description",
-      content: "Welcome to Remix on Cloudflare!",
+      content: "Welcome to monolog!",
     },
   ];
 };
@@ -21,29 +21,7 @@ export default function Index() {
   const data = useLoaderData<typeof loader>();
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">{data}</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-      </ul>
+      <h1 className="text-3xl">{data}</h1>{" "}
     </div>
   );
 }
